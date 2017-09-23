218829738e1f033976cf199c484d78075052e0fa
Matt Lim <mlim@caltech.edu>
2017-09-23 16:31:07 -0700

    Minor networking fixes. Should actually work with client

--- s_main.lua
+++ s_main.lua
@@ -4,25 +4,26 @@ local Player = require("entities.s_player")
 local udp = socket.udp()
 
 udp:settimeout(0)
--- udp:setoption('broadcast', true)
-udp:setsockname('192.168.1.12', 12345)
-udp:setpeername('192.168.1.255', 12345)
+udp:setsockname('*', 12345)
 
-local data, msg_or_ip, port_or_nil, cmd, params, dt, current_time
+local data, ip, port, cmd, params, dt, current_time
 local broadcast_interval = 0.1
 local previous_time = socket.gettime()
 local previous_broadcast = socket.gettime()
 
--- TODO add dt
 print "Beginning server loop."
 while true do
+  -- Do time calculations at beginning
   current_time = socket.gettime()
   dt = current_time - previous_time
-  data, msg_or_ip, port_or_nil = udp:receive()
-  print(data, msg_or_ip, port_or_nil)
-  -- TODO: check msg_or_ip and port_or_nil
+
+  -- Get data and client location
+  data, ip, port = udp:receivefrom()
+  is_connected = data and ip and port
+  print(string.format("data = %s, ip = %s, port = %s", data, ip, port))
+
   -- TODO: broadcast update instead of sending it per request
-  if data then
+  if is_connected then
     -- more of these funky match paterns!
     ent_id, cmd, params = data:match("^(%S*) (%S*) (.*)")
     ent_id = tonumber(ent_id)
@@ -46,7 +47,7 @@ while true do
         math.randomseed(os.time())
         id = math.random(99999)
       until id ~= 0 and not ents:get_ent(id)
-      new_player = Player(50, 50, 32, 32, udp, msg_or_ip, port_or_nil, id)
+      new_player = Player(50, 50, 32, 32, udp, ip, port, id)
       ents:add(id, new_player)
       print("send_spawn_info call")
       new_player:send_spawn_info()
@@ -55,14 +56,15 @@ while true do
     else
       print("unrecognised command:", cmd)
     end
-  elseif msg_or_ip ~= 'timeout' then
+  elseif ip ~= 'timeout' then
     error("Unknown network error: "..tostring(msg))
   end
 
-  if current_time - previous_broadcast > broadcast_interval then
-    udp:send('ping')
-    previous_broadcast = current_time
-  end
+  -- if current_time - previous_broadcast > broadcast_interval then
+  --   print('pinging clients...')
+  --   udp:sendto('ping', ip, port)
+  --   previous_broadcast = current_time
+  -- end
 
   previous_time = current_time
   socket.sleep(0.01)
